993171d867f0b6d47bc77406ef95707dcd2bfc02
“AnupamSistla” <sistlaanupam@gmail.com>
2026-07-08 19:24:47 -0500

    memo change

--- ninja-and-friends/memoization.py
+++ ninja-and-friends/memoization.py
@@ -3,6 +3,9 @@ class Solution:
         if j1 < 0 or j1 >= c or j2 < 0 or j2 >= c:
             return float("-inf")
         
+        if dp[i][j1][j2] != -1:
+            return dp[i][j1][j2]
+        
         if i == r-1:
             if j1 == j2:
                 return grid[i][j1]
@@ -10,9 +13,6 @@ class Solution:
             else: 
                 return grid[i][j1] + grid[i][j2]
         
-        if dp[i][j1][j2] != -1:
-            return dp[i][j1][j2]
-
         maxSum = float("-inf")
         delta = [-1, 0, 1]
 
