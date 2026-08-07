e4ba8d6126efa3c6b63704cd2b68e23d3e10ff1a
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-06 23:41:03 -0500

    tested myself on unique paths

--- test.py
+++ test.py
@@ -1,54 +1,23 @@
 class Solution:
-    def foo(self, index, heights, k, dp):
-        if dp[index] != -1:
-            return dp[index]
+    def uniquePaths(self, m, n):
+        prev = [0] * m
 
-        if index == 0:
-            return 0
+        for i in range(n):
+            curr = [0] * m
+            for j in range(m):
+                if i == 0 and j == 0:
+                    curr[j] = 1
 
-        minCost = float("inf")
-        for jump in range(1, k + 1):
-            currCost = float("inf")
+                elif j > 0:
+                    curr[j] = prev[j] + curr[j-1]
 
-            if index - jump >= 0:
-                currCost = abs(heights[index] - heights[index - jump]) + self.foo(index - jump, heights, k, dp)
-
-            minCost = min(minCost, currCost)
-
-        dp[index] = minCost
-        return dp[index]
-
-    def frogJump(self, heights, k):
-        n = len(heights)
-        dp = [-1]*n
-
-        dp[0] = 0
-
-        for index in range(1, n):
-            minCost = float("inf")
-            for jump in range(1, k + 1):
-                currCost = float("inf")
+                else:
+                    curr[j] = prev[j]
+            prev = curr
+        return prev[m-1]
     
-                if index - jump >= 0:
-                    currCost = abs(heights[index] - heights[index - jump]) + dp[index - jump]
-    
-                minCost = min(minCost, currCost)
-            dp[index] = minCost
-
-        return dp[n-1]
-
-
 if __name__ == "__main__":
-    test1 = [10, 5, 20, 0, 15]
-    test2 = [15, 4, 1, 14, 15]    
-    test3 = [15, 4, 1, 14, 15]
-
     dummy = Solution()
-    res1 = dummy.frogJump(test1, 2)
-    print(res1)
-
-    res2 = dummy.frogJump(test2, 3)
-    print(res2)
-
-    res3 = dummy.frogJump(test3, 4)
-    print(res3)
+    
+    print(dummy.uniquePaths(3, 2))
+    print(dummy.uniquePaths(2, 4))
