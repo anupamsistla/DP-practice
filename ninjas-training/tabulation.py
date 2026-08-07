9ba2b7ee7c87d95312797b4957bc980a2efbb7b5
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-06 23:08:12 -0500

    tested myself on ninjas training

---
+++ ninjas-training/tabulation.py
@@ -0,0 +1,38 @@
+class Solution:
+    def ninjaTraining(self, matrix):
+        n = len(matrix)
+        dp = [[0]*4 for _ in range(n)]
+
+        for index in range(3):
+            maxPoints = float("-inf")
+            for j in range(3):
+                if index == j:
+                    continue
+                maxPoints = max(maxPoints, matrix[0][j])
+            dp[0][index] = maxPoints
+
+        for i in range(1, n):
+            for j in range(0, 4):
+                maxPoints = float("-inf")
+                for index in range(3):
+                    if index == j:
+                        continue 
+                    currPoints = matrix[i][index] + dp[i-1][index]
+                    maxPoints = max(currPoints, maxPoints)
+        
+                dp[i][j] = maxPoints
+        return dp[n-1][3]
+
+# Time complexity: O(n * 4 * 3)
+# Space complexity: O(n * 4  * 3)
+
+if __name__ == "__main__":
+    test1 = [[10, 30, 70], [20, 50, 80], [30, 60, 90]]
+    test2 = [[70, 40, 10], [180, 20, 5], [200, 60, 30]]
+    test3 = [[20, 10, 10], [20, 10, 10], [20, 30, 10]]
+
+    dummy = Solution()
+
+    print(dummy.ninjaTraining(test1))
+    print(dummy.ninjaTraining(test2))
+    print(dummy.ninjaTraining(test3))
