98978f66f1c357931f2de9582ca0023513017a12
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-07 23:53:17 -0500

    tested myself on min-path-sum actual

--- test.py
+++ test.py
@@ -1,23 +1,29 @@
+from typing import List
 class Solution:
-    def uniquePaths(self, m, n):
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
         prev = [0] * m
+
+        prev[0] = grid[0][0]
 
         for i in range(n):
             curr = [0] * m
+            curr[0] = grid[0][0]
             for j in range(m):
                 if i == 0 and j == 0:
-                    curr[j] = 1
+                    continue
 
-                elif j > 0:
-                    curr[j] = prev[j] + curr[j-1]
+                up = grid[i][j] + prev[j] if i > 0 else float("inf")
+                left = grid[i][j] + curr[j-1] if j > 0 else float('inf')
+        
+                curr[j] = min(up, left)     
 
-                else:
-                    curr[j] = prev[j]
-            prev = curr
+            prev = curr          
+
         return prev[m-1]
-    
+
 if __name__ == "__main__":
     dummy = Solution()
-    
-    print(dummy.uniquePaths(3, 2))
-    print(dummy.uniquePaths(2, 4))
+
+    print(dummy.minPathSum([[5,9,6],[11,5,2]]))
+    print(dummy.minPathSum([[1,2,3],[4,5,6]]))
