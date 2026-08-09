52099e3df968385e6b18954dbac66f11f212fa0a
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-08 22:50:18 -0500

    tested myself on min-path-sum triangle

--- test.py
+++ test.py
@@ -1,29 +1,51 @@
-from typing import List
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        n, m = len(grid), len(grid[0])
+    def foo(self, i, j, triangle, dp):
+        if i == 0 and j == 0:
+            return triangle[0][0]
+
+        if j < 0:
+            return float("inf")
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        up = float("inf")
+
+        if i != j:
+            up = triangle[i][j] + self.foo(i-1, j, triangle, dp)
+
+        upLeft = triangle[i][j] + self.foo(i-1, j-1, triangle, dp)
+        dp[i][j] = min(up, upLeft)
+
+        return dp[i][j]
+
+    def minTriangleSum(self, triangle):
+        n = len(triangle)
+        m = len(triangle[n-1])
         prev = [0] * m
 
-        prev[0] = grid[0][0]
+        minSum = float("inf")
 
-        for i in range(n):
+        prev[0] = triangle[0][0]
+
+        for i in range(1, n):
             curr = [0] * m
-            curr[0] = grid[0][0]
-            for j in range(m):
-                if i == 0 and j == 0:
-                    continue
+            for j in range(len(triangle[i])):
+                up = float("inf")
+                
+                if i != j:
+                    up = triangle[i][j] + prev[j]
 
-                up = grid[i][j] + prev[j] if i > 0 else float("inf")
-                left = grid[i][j] + curr[j-1] if j > 0 else float('inf')
-        
-                curr[j] = min(up, left)     
+                upLeft = triangle[i][j] + prev[j-1] if j > 0 else float("inf")
+                curr[j] = min(up, upLeft)
+            prev = curr
 
-            prev = curr          
-
-        return prev[m-1]
+        for j in range(m):
+            minSum = min(minSum, prev[j])
+        return minSum
 
 if __name__ == "__main__":
     dummy = Solution()
-
-    print(dummy.minPathSum([[5,9,6],[11,5,2]]))
-    print(dummy.minPathSum([[1,2,3],[4,5,6]]))
+    print(dummy.minTriangleSum([[1], [1, 2], [1, 2, 4]]))
+    print(dummy.minTriangleSum([[1], [4, 7], [4,10, 50], [-50, 5, 6, -100]]))
+    
