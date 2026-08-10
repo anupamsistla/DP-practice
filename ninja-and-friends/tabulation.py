bd2e914cbbe5c05e38d98119ccc807eaa10c78b5
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-10 11:01:40 -0500

    added tabulation and optimal for ninja and friends

---
+++ ninja-and-friends/tabulation.py
@@ -0,0 +1,35 @@
+class Solution:    
+    def aliceAndBob(self, n, m, g):
+        dp = [[[0]*m for _ in range(m)] for _ in range(n)]
+
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 == j2:
+                    dp[n-1][j1][j2] = g[n-1][j1]
+                else:
+                    dp[n-1][j1][j2] = g[n-1][j1] + g[n-1][j2]
+
+        for i in range(n-2, -1, -1):
+            for j1 in range(m-1, -1, -1):
+                for j2 in range(m-1, -1, -1):
+                    dirs = [-1, 0, 1]
+                    maxSum = float("-inf")
+            
+                    for x in dirs:
+                        for y in dirs:
+                            currSum = g[i][j1] + g[i][j2] if j1 != j2 else g[i][j1]
+
+                            if j1 + x in range(m) and j2 + y in range(m):
+                                maxSum = max(maxSum, currSum + dp[i + 1][j1 + x][j2 + y])
+            
+                    dp[i][j1][j2] = maxSum
+
+        return dp[0][0][m-1]
+
+# Time complexity: O(n * m^2)
+# Space complexity: O(n * m^2) 
+
+if __name__ == "__main__":
+    dummy = Solution()
+    print(dummy.aliceAndBob(3, 4, [[2, 3, 1, 2], [3, 4, 2, 2], [5, 6, 3, 5]]))
+    print(dummy.aliceAndBob(2, 3, [[4, 1, 2], [7, 3, 5]]))
