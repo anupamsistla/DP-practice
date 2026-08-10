b56afe095f3528ffb30d37bd0708b7686fcc49f9
“AnupamSistla” <sistlaanupam@gmail.com>
2026-08-10 00:28:07 -0500

    tested myself on ninjas and friends, finished optimal aswell

--- test.py
+++ test.py
@@ -1,51 +1,62 @@
 class Solution:
-    def foo(self, i, j, triangle, dp):
-        if i == 0 and j == 0:
-            return triangle[0][0]
+    def foo(self, i, j1, j2, g, dp):
+        if j1 < 0 or j2 < 0 or j1 >= len(g[0]) or j2 >= len(g[0]):
+            return float("-inf")
 
-        if j < 0:
-            return float("inf")
+        if dp[i][j1][j2] != -1:
+            return dp[i][j1][j2]
 
-        if dp[i][j] != -1:
-            return dp[i][j]
+        if i == 0:
+            if j1 != j2:
+                return g[i][j1] + g[i][j2]
 
-        up = float("inf")
+            else:
+                return g[i][j1]
 
-        if i != j:
-            up = triangle[i][j] + self.foo(i-1, j, triangle, dp)
 
-        upLeft = triangle[i][j] + self.foo(i-1, j-1, triangle, dp)
-        dp[i][j] = min(up, upLeft)
+        dirs = [-1, 0, 1]
 
-        return dp[i][j]
+        maxSum = float("-inf")
+        for x in dirs:
+            for y in dirs:
+                curr = g[i][j1] + g[i][j2] if j1 != j2 else g[i][j1]
+                maxSum = max(maxSum, curr + self.foo(i-1, j1 + x, j2 + y, g, dp))
 
-    def minTriangleSum(self, triangle):
-        n = len(triangle)
-        m = len(triangle[n-1])
-        prev = [0] * m
+        dp[i][j1][j2] = maxSum
+        return maxSum
+        
+    def aliceAndBob(self, n, m, g):
+        prev = [[0] * m for _ in range(m)]
 
-        minSum = float("inf")
-
-        prev[0] = triangle[0][0]
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 != j2:
+                    prev[j1][j2] = g[0][j1] + g[0][j2]
+                else:
+                    prev[j1][j2] = g[0][j1]
 
         for i in range(1, n):
-            curr = [0] * m
-            for j in range(len(triangle[i])):
-                up = float("inf")
-                
-                if i != j:
-                    up = triangle[i][j] + prev[j]
+            curr = [[0] * m for _ in range(m)]
+            for j1 in range(m-1, -1, -1):
+                for j2 in range(m-1, -1, -1):
+                    dirs = [-1, 0, 1]
+                    
+                    maxSum = float("-inf")
+                    for x in dirs:
+                        for y in dirs:
+                            currSum = g[i][j1] + g[i][j2] if j1 != j2 else g[i][j1]
 
-                upLeft = triangle[i][j] + prev[j-1] if j > 0 else float("inf")
-                curr[j] = min(up, upLeft)
+                            if j1 + x in range(m) and j2 + y in range(m):
+                                maxSum = max(maxSum, currSum + prev[j1 + x][j2 + y]) 
+            
+                    curr[j1][j2] = maxSum
             prev = curr
 
-        for j in range(m):
-            minSum = min(minSum, prev[j])
-        return minSum
+        return prev[0][m-1]
+
+        # Your code goes here
 
 if __name__ == "__main__":
     dummy = Solution()
-    print(dummy.minTriangleSum([[1], [1, 2], [1, 2, 4]]))
-    print(dummy.minTriangleSum([[1], [4, 7], [4,10, 50], [-50, 5, 6, -100]]))
-    
+    print(dummy.aliceAndBob(3, 4, [[2, 3, 1, 2], [3, 4, 2, 2], [5, 6, 3, 5]]))
+    print(dummy.aliceAndBob(2, 3, [[4, 1, 2], [7, 3, 5]]))
