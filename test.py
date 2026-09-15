ca42a4dc5b1e606ba6e02cb993ccff26859504f5
“AnupamSistla” <sistlaanupam@gmail.com>
2026-09-15 12:11:07 -0500

    revised distinct subsequences

--- test.py
+++ test.py
@@ -1,40 +1,44 @@
 class Solution:
-    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        n = len(str1)
-        m = len(str2)
-        dp = [[0]*(m+1) for _ in range(n+1)]
+    def foo(self, i, j, s, t, dp):
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        if j == 0:
+            return 1
+
+        if i == 0:
+            return 0
+
+        if s[i-1] == t[j-1]:
+            dp[i][j] = self.foo(i-1, j-1, s, t, dp) + self.foo(i-1, j, s, t, dp)
+            return dp[i][j]
+
+        dp[i][j] = self.foo(i-1, j, s, t, dp)
+        return dp[i][j]
+        
+    def distinctSubsequences(self, s, t):
+        n = len(s)
+        m = len(t)
+
+        prev = [0]*(m+1)
+        prev[0] = 1
 
         for i in range(1, n+1):
+            curr = [0]*(m+1)
+            prev[0] = 1
             for j in range(1, m+1):
-                if str1[i-1] == str2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                if s[i-1] == t[j-1]:
+                    curr[j] = prev[j-1] + prev[j]
                 
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    curr[j] = prev[j]   
+            prev = curr
 
-        i, j = n, m
-        res = ""
-
-        while i > 0 and j > 0:
-            if str1[i-1] == str2[j-1]:
-                res = str1[i-1] + res
-                i -= 1
-                j -= 1
-
-            else:
-                if dp[i-1][j] >= dp[i][j-1]:
-                    res = str1[i-1] + res
-                    i -= 1
-
-                else:
-                    res = str2[j-1] + res
-                    j -= 1
-        return str1[:i] + str2[:j] + res
+        return prev[m]
         
 
 if __name__ == "__main__":
     dummy = Solution()
-    print(dummy.shortestCommonSupersequence("brute", "groot"))
-    print(dummy.shortestCommonSupersequence("mno", "nop"))
-    print(dummy.shortestCommonSupersequence("dynamic","program"))
-    print(dummy.shortestCommonSupersequence("coding", "ninjas"))
+    print(dummy.distinctSubsequences("babgbag", "bag"))
+    print(dummy.distinctSubsequences("axbxax", "axa"))
+    print(dummy.distinctSubsequences("rabbbit", "rabbit"))
