d4e6a8fd64476d5baed80a8fcb6182cae7fbb555
“AnupamSistla” <sistlaanupam@gmail.com>
2026-09-17 12:21:38 -0500

    revised wildcard matching

--- test.py
+++ test.py
@@ -1,55 +1,75 @@
 class Solution:
-    def foo(self, i, j, start, target, dp):
+    def foo(self, i, j, str, pat, dp):
+        if i == 0 and j == 0:
+            return True
+
+        if i >= 1 and j == 0:
+            for ii in range(i):
+                if pat[ii] != "*":
+                    return False
+
+            return True
+
+        if j >= 1 and i == 0:
+            return False
+
         if dp[i][j] != -1:
             return dp[i][j]
-        
-        if i == 0:
-            return j
 
-        if j == 0:
-            return i
-
-        if start[i-1] == target[j-1]:
-            dp[i][j] = self.foo(i-1, j-1, start, target, dp)
+        if pat[i-1] == str[j-1] or pat[i-1] == "?":
+            dp[i][j] = self.foo(i-1, j-1, str, pat, dp)
             return dp[i][j]
-        
-        insert = 1 + self.foo(i, j-1, start, target, dp)
-        delete = 1 + self.foo(i-1, j, start, target, dp)
-        replace = 1 + self.foo(i-1, j-1, start, target, dp)
 
-        dp[i][j] = min(insert, delete, replace)
+        if pat[i-1] == "*":
+            dp[i][j] = self.foo(i-1, j, str, pat, dp) or self.foo(i, j-1, str, pat, dp)
+            return dp[i][j]
+
+        dp[i][j] = False
         return dp[i][j]
+        
 
-    def editDistance(self, start, target):
-        n = len(start)
-        m = len(target)
-        prev = [0]*(m+1)
+    def wildCard(self, str: str, pat: str) -> bool:
+        n = len(pat)
+        m = len(str)
 
-        for j in range(m+1):
-            prev[j] = j
+        prev = [False] * (m+1)
+
+        prev[0] = True
+
+        for j in range(1, m+1):
+            prev[j] = False
 
 
         for i in range(1, n+1):
-            curr = [0] * (m + 1)
-            curr[0] = i
+            curr = [0] * (m+1)
+            flag = True
+            for ii in range(i):
+                if pat[ii] != '*':
+                    flag = False
+
+            curr[0] = flag
             for j in range(1, m+1):
-                if start[i-1] == target[j-1]:
+                if pat[i-1] == str[j-1] or pat[i-1] == '?':
                     curr[j] = prev[j-1]
-
+                
+        
+                elif pat[i-1] == '*':
+                    curr[j] = prev[j] or curr[j-1]
+                
                 else:
-                    insert = 1 + curr[j-1]
-                    delete = 1 + prev[j]
-                    replace = 1 + prev[j-1]
-
-                    curr[j] = min(insert, delete, replace)
+                    curr[j] = False
 
             prev = curr
 
         return prev[m]
-            
+                
+
+
 
 if __name__ == "__main__":
     dummy = Solution()
-    print(dummy.editDistance("horse", "ros"))
-    print(dummy.editDistance("intention", "execution"))
-    print(dummy.editDistance("abcdefg", "azced"))
+    print(dummy.wildCard("abdefcd", "ab*cd"))
+    print(dummy.wildCard("xaylmz", "x?y*z"))
+    print(dummy.wildCard("xyza", "x*z"))
+    print(dummy.wildCard("abc", "a**bc"))
+    print(dummy.wildCard("", "****"))
